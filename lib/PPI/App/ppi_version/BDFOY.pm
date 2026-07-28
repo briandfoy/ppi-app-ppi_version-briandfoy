0917962a10d8191f2a4bad466b39dfaf93483e35
brian d foy <briandfoy@pobox.com>
2026-07-28 03:00:38 -0400

    bump version

--- lib/PPI/App/ppi_version/BDFOY.pm
+++ lib/PPI/App/ppi_version/BDFOY.pm
@@ -1,7 +1,7 @@
 package PPI::App::ppi_version::BDFOY;
 use parent qw(PPI::App::ppi_version::BRIANDFOY);
 
-our $VERSION = '1.008';
+our $VERSION = '1.009';
 
 1;
 
--- lib/PPI/App/ppi_version/BRIANDFOY.pm
+++ lib/PPI/App/ppi_version/BRIANDFOY.pm
@@ -42,7 +42,7 @@ use File::Find::Rule       ();
 use File::Find::Rule::Perl ();
 use Term::ANSIColor;
 
-our $VERSION = '1.008';
+our $VERSION = '1.009';
 
 #####################################################################
 # Main Methods
