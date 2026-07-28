af880af2a1c11286eedf3d43d3b5a333425c5154
brian d foy <briandfoy@pobox.com>
2026-07-28 01:06:44 -0400

    bump version from 1.007 to 1.008

--- lib/PPI/App/ppi_version/BRIANDFOY.pm
+++ lib/PPI/App/ppi_version/BRIANDFOY.pm
@@ -42,7 +42,7 @@ use File::Find::Rule       ();
 use File::Find::Rule::Perl ();
 use Term::ANSIColor;
 
-our $VERSION = '1.007';
+our $VERSION = '1.008';
 
 #####################################################################
 # Main Methods
